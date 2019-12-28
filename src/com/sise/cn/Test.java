719a19896a33d3c0edfc531fa95c9141ff8c7f32
wei <wei@163.com>
2019-12-28 11:32:05 +0800

    laowang--version1.2

--- src/com/sise/cn/Test.java
+++ src/com/sise/cn/Test.java
@@ -1,5 +1,18 @@
 package com.sise.cn;
 
 public class Test {
+    public static void main(String[] args){
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+        System.out.print("老王是我孙子。。。。。。。。。。");
+
+
+    }
 
 }
